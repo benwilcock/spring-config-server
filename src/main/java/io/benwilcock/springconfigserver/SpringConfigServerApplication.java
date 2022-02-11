5619ea4cbbfdbc1edfaf2d634ae545de5bb971ff
benwilcock <benwilcock@gmail.com>
2022-02-11 11:26:06 +0000

    added security config to app

--- src/main/java/io/benwilcock/springconfigserver/SpringConfigServerApplication.java
+++ src/main/java/io/benwilcock/springconfigserver/SpringConfigServerApplication.java
@@ -1,13 +1,33 @@
 package io.benwilcock.springconfigserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class SpringConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringConfigServerApplication.class, args);
+	}
+
+	@Bean
+	public HttpTraceRepository htttpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
+	}
+
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll()  
+				.and().csrf().disable();
+		}
 	}
 
 }
